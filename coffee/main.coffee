f0a04d4caf56358c311e2ceca575ca4751a2c4e4
Ken Lim <kenneth.lim@guardian.co.uk>
2012-04-01 01:49:04 +0100

    Working display.

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -2,5 +2,38 @@ log = (args...) ->
     console.log.apply console, args if console.log?
 
 $( () ->
-	log("hello world!")
+ChecklistDoc = Backbone.Model.extend(
+	initialize :  (stuff) ->
+		this.url = stuff.url
+)
+
+Checklist = Backbone.Model.extend()
+
+Item = Backbone.Model.extend()
+
+DocumentView = Backbone.View.extend(
+	model : ChecklistDoc
+
+	el : "#documentView"
+
+	initialize : () ->
+		this.model.bind('change', this.render, this)
+
+		this.model.fetch(
+			success : () -> log("A winner is you")
+			error : (model, response) -> log("Error fetching DocumentView", model, response)
+		)
+
+	template : _.template($('#document-template').html())
+
+	render : () -> 
+		log("finish!")
+		$(this.el).html(this.template(this.model.toJSON()))
+		return this
+)
+
+
+appView = new DocumentView( model : new ChecklistDoc( url : "who-surgical-checklist.json"))
+
+
 )
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function() {
-  var log,
+  var Checklist, ChecklistDoc, DocumentView, Item, appView, log,
     __slice = Array.prototype.slice;
 
   log = function() {
@@ -8,8 +8,34 @@
     if (console.log != null) return console.log.apply(console, args);
   };
 
-  $(function() {
-    return log("hello world!");
-  });
+  $(function() {}, ChecklistDoc = Backbone.Model.extend({
+    initialize: function(stuff) {
+      return this.url = stuff.url;
+    }
+  }), Checklist = Backbone.Model.extend(), Item = Backbone.Model.extend(), DocumentView = Backbone.View.extend({
+    model: ChecklistDoc,
+    el: "#documentView",
+    initialize: function() {
+      this.model.bind('change', this.render, this);
+      return this.model.fetch({
+        success: function() {
+          return log("A winner is you");
+        },
+        error: function(model, response) {
+          return log("Error fetching DocumentView", model, response);
+        }
+      });
+    },
+    template: _.template($('#document-template').html()),
+    render: function() {
+      log("finish!");
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    }
+  }), appView = new DocumentView({
+    model: new ChecklistDoc({
+      url: "who-surgical-checklist.json"
+    })
+  }));
 
 }).call(this);
